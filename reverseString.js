ff257d9e00a87ce0b2aac4a19f48576f69986313
Naveen-017 <naveenkaniganti017@gmail.com>
2024-04-27 16:14:56 +0530

    new code added

---
+++ reverseString.js
@@ -0,0 +1,8 @@
+let h = 'naveen';
+let str  = h.toString();
+let rev = "";
+for(let i =0; i<str.length; i++){
+    rev = str[i] + rev
+
+}
+console.log(rev)
--- test.js
+++ test.js
@@ -4,3 +4,6 @@ let b = "ravi";
 let c = a+b;
 
 console.log(c);
+
+
+console.log("37" - 7)
